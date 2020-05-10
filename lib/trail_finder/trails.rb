3c4ca46fe8be7c828fb47b3bca73dcb223b7914d
satodd1967 <seth.todd@outlook.com>
2020-05-10 11:56:27 -0500

    Experimenting with the JSON file in trails and finally was able to extract out the data points that are required.  Now need to find an way to pull lat and long to drive the JSON file

--- lib/trail_finder/trails.rb
+++ lib/trail_finder/trails.rb
@@ -6,22 +6,23 @@ require "pry"
 # doc = Nokogiri::HTML(open("https://www.mtbproject.com/data/get-trails?lat=33.124&lon=-117.080&maxDistance=10&key=200752494-3cc116808ab25b0826c7c50c3d42c825"))
 # puts doc
     
-    # lat = 33.124
-    # lon = -117.080
-    # distance = 5
-    # api_call = "https://www.mtbproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
-    # doc = Nokogiri::HTML(open(api_call))
-    # new_doc = JSON.parse(doc)
-    # puts new_doc.collect {|trails| trails}.first
+    lat = 33.124
+    lon = -117.080
+    distance = 5
+    api_call = "https://www.mtbproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
+    doc = Nokogiri::HTML(open(api_call))
+    new_doc = JSON.parse(doc)
+    # puts new_doc["trails"].class
+    puts new_doc["trails"].map {|trails| trails["name"]}
     
-    def get_trails
-        lat = 33.124
-        lon = -117.080
-        distance = 5
-        api_call = "https://www.mtbproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
-        doc = Nokogiri::HTML(open(api_call))
-        new_doc = JSON.parse(doc)
-        binding.pry
-    end
+    # def get_trails
+    #     lat = 33.124
+    #     lon = -117.080
+    #     distance = 5
+    #     api_call = "https://www.mtbproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
+    #     doc = Nokogiri::HTML(open(api_call))
+    #     new_doc = JSON.parse(doc)
+    #     puts new_doc
+    # end
 
-    
+   
