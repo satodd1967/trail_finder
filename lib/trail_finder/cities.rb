1bdf4eef9ef90f7f00fe30972f52e37bdf88485c
satodd1967 <seth.todd@outlook.com>
2020-05-10 15:38:32 -0500

    got the cities data pulling and working

--- lib/trail_finder/cities.rb
+++ lib/trail_finder/cities.rb
@@ -1,8 +1,16 @@
 
 require "nokogiri"
 require "open-uri"
+require "json"
 
-doc = Nokogiri::HTML(open("https://gist.github.com/Miserlou/c5cd8364bf9b2420bb29"))
-puts doc.css(".pl-s").css(".pl-pds").value
+api_call = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=1000-largest-us-cities-by-population-with-geographic-coordinates&q=&rows=1000&sort=-rank&facet=city&facet=state"
+doc = Nokogiri::HTML(open(api_call))
+new_doc = JSON.parse(doc)
+
+#Gets to the level needed.  Will need to itterate over this due to the fact that below records is an array
+# puts new_doc["records"][1]["fields"]["city"]
+
+puts new_doc["records"].map {|arrays| arrays["fields"]["coordinates"]}.first
+
 
 
