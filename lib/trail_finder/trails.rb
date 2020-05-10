fca489f6ea329945a4474e2b1883ca7f1ab6cb4c
satodd1967 <seth.todd@outlook.com>
2020-05-10 15:49:08 -0500

    clearned up all the random code in trails and cities and commented everything out to continue to stub out the program

--- lib/trail_finder/trails.rb
+++ lib/trail_finder/trails.rb
@@ -2,9 +2,6 @@ require "nokogiri"
 require "open-uri"
 require "json"
 require "pry"
-
-# doc = Nokogiri::HTML(open("https://www.mtbproject.com/data/get-trails?lat=33.124&lon=-117.080&maxDistance=10&key=200752494-3cc116808ab25b0826c7c50c3d42c825"))
-# puts doc
     
     lat = 33.124
     lon = -117.080
@@ -12,7 +9,6 @@ require "pry"
     api_call = "https://www.mtbproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
     doc = Nokogiri::HTML(open(api_call))
     new_doc = JSON.parse(doc)
-    # puts new_doc["trails"].class
     puts new_doc["trails"].map {|trails| trails["name"]}
     
     # def get_trails
