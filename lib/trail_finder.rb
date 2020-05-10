aae34b099957d8471f2ad5476b92332716b3f578
satodd1967 <seth.todd@outlook.com>
2020-05-09 19:16:01 -0500

    updated the call method and added the pupular_cities method with fake data

--- lib/trail_finder.rb
+++ lib/trail_finder.rb
@@ -1,7 +1,3 @@
 require_relative "trail_finder/version"
 require_relative "trail_finder/cli"
 
-module TrailFinder
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/trail_finder/cli.rb
+++ lib/trail_finder/cli.rb
@@ -3,7 +3,18 @@
 class TrailFinder::CLI
 
     def call
-        puts "hello riders"
+        puts "Welcome Riders!"
+        puts "Ride Fast Crash Less is a tool that allows you to input your city and get back all the best trails in your area."
+        puts "Below is a list of the most popular cities.  You can either choose one of those by inputing the corresponding number or you can enter your local city."
+        popular_cities
+        puts "Where would you like to ride?"
+    end
+
+    def popular_cities
+        puts "City1"
+        puts "City2"
+        puts "City3"
+        puts "City4"
     end
 
 end
