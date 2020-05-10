a96943d95cf4f3ddb9c0e19cdc1efc0bd746cfa9
satodd1967 <seth.todd@outlook.com>
2020-05-09 21:24:06 -0500

    validated MTB project API with manipulated call string

--- lib/trail_finder.rb
+++ lib/trail_finder.rb
@@ -1,3 +1,4 @@
 require_relative "trail_finder/version"
 require_relative "trail_finder/cli"
-
+require_relative "trail_finder/trails"
+require_relative "trail_finder/cities"
---
+++ lib/trail_finder/trails.rb
@@ -0,0 +1,13 @@
+require "nokogiri"
+require "open-uri"
+
+# doc = Nokogiri::HTML(open("https://www.mtbproject.com/data/get-trails?lat=33.124&lon=-117.080&maxDistance=10&key=200752494-3cc116808ab25b0826c7c50c3d42c825"))
+# puts doc
+    
+    lat = 33.124
+    lon = -117.080
+    distance = 5
+    api_call = "https://www.mtbproject.com/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=#{distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
+    doc = Nokogiri::HTML(open(api_call))
+    puts doc
+    
