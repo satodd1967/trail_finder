e363af59f419e5f41811c269aa68b0bde3b99cfa
satodd1967 <seth.todd@outlook.com>
2020-05-10 23:03:31 -0500

    set the distance variable to an optional variable that will defualt to 100 miles if no variable is given

--- lib/trail_finder/trails.rb
+++ lib/trail_finder/trails.rb
@@ -24,5 +24,8 @@ class Get_Trails
         @distance = distance
         @trail_data = []
     end
-    
+
+    def api_call
+        url = "https://www.mtbproject.com/data/get-trails?lat=#{@lat}&lon=#{@lon}&maxDistance=#{@distance}&key=200752494-3cc116808ab25b0826c7c50c3d42c825"
+
 end   
