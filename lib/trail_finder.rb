6c75a0fda44862a2515e1958e8245a18e5570370
satodd1967 <seth.todd@outlook.com>
2020-05-12 19:02:23 -0500

    added development dependancies for pry, nokogiri and json into the gemspec file and to the trail_finder file in lib.  I know that nokogiri and json should not be development dependancies, but rather dependancies, but they don't work that way will need to figure this out later

--- lib/trail_finder.rb
+++ lib/trail_finder.rb
@@ -1,5 +1,9 @@
+require "nokogiri"
+require "pry"
+require "json"
 require_relative "trail_finder/version"
 require_relative "trail_finder/cli"
 require_relative "trail_finder/trails"
 require_relative "trail_finder/cities"
-
+require_relative "trail_finder/scraper_city"
+require_relative "trail_finder/scraper_trails"
