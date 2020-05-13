7ba9ef422dee76b5e783dbbcaeec47187acf257e
satodd1967 <seth.todd@outlook.com>
2020-05-12 20:17:32 -0500

    braught the cities api call into the scraper_city.rb file and set it up with a class variable called @@doc that is set to an empty hash.  When the class is called with the api_call method it makes @@doc the json parsed hash from the API call.  I will try and refactor this so that the api_call method is called upon initialize and see if that works.

---
+++ lib/trail_finder/scraper_city.rb
@@ -0,0 +1,17 @@
+
+class TrailFinder::City_Scrape
+
+    @@doc = {}
+
+    #pulls data from the MTB project api and then uses JSON parse to convert it to a usable hash.  Returns that hash.
+    def api_call
+        @@doc = {}
+        url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=1000-largest-us-cities-by-population-with-geographic-coordinates&q=&rows=1000&sort=-rank&facet=city&facet=state"
+        data = Nokogiri::HTML(open(url))
+        @@doc =  JSON.parse(data)
+    end
+
+    def self.doc
+        @@doc
+    end
+end
