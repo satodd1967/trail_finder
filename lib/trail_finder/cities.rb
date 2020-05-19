7b8b463f876faca16ac6c2e6b169c78194e7b2a4
satodd1967 <seth.todd@outlook.com>
2020-05-18 20:54:53 -0500

    refactored every method in the app to fit the pattern that it should have been using objects vs. just pulling data from hashes.  I have it all working now, but need to clean up the outputs from get trails and get_trail_details.

--- lib/trail_finder/cities.rb
+++ lib/trail_finder/cities.rb
@@ -3,6 +3,7 @@ class TrailFinder::Cities
     attr_accessor :city, :coordinates, :state, :rank, :growth_from_2000_to_2013, :population
    
     @@all = []
+    @@f_cities = []
 
     def initialize(city_list)
       city_list.each {|key, value| self.send(("#{key}="), value)}
@@ -11,6 +12,34 @@ class TrailFinder::Cities
 
     def self.all
         @@all
+    end
+
+    def self.f_cities
+        @@f_cities
+    end
+
+    def self.featured_cities
+        sample = @@all.sample(5)
+        sample.each.with_index(1) do |cities, i|
+            puts "#{i}. #{cities.city}, #{cities.state}"
+        end
+        sample.each do |cities|
+            @@f_cities << cities
+        end
+    end
+
+    def self.get_city(city, state = nil, distance = nil)
+        if city.to_i.between?(1, 5)
+            input = city.to_i - 1
+            item = @@f_cities[input]
+            # lat = @@f_cities[input].coordinates[0]
+            # lon = @@f_cities[input].coordinates[1]
+        else 
+            item = @@all.select {|cities| cities.city == city && cities.state = state}[0]
+            # lat = city[0].coordinates[0]
+            # lon = city[0].coordinates[1]
+        end
+        item
     end
 
 end
--- lib/trail_finder/cli.rb
+++ lib/trail_finder/cli.rb
@@ -1,21 +1,167 @@
 class TrailFinder::CLI
 
-    def call
-       TrailFinder::Trail_Scrape.api_call(33.123, -117.080, 2)
-       puts TrailFinder::Trails.all.first 
+    def initialize
+        colors
     end
 
-    # def featured_cities
-    #     cities = TrailFinder::Featured_Cities.new
-    #     @f_cities = cities.get_featured
-    #     puts "1. #{@f_cities[0]}"
-    #     puts "2. #{@f_cities[1]}"
-    #     puts "3. #{@f_cities[2]}"
-    #     puts "4. #{@f_cities[3]}"
-    #     puts "5. #{@f_cities[4]}"
-    # end
-         
+    def call
+        puts "Loading..."
+        TrailFinder::City_Scrape.api_call
+        intro
+        featured_cities
+        controller_main
+        over
+    end
+
+    def colors
+        @red = "\u001b[31m"
+        @green = "\u001b[32m"
+        @yellow = "\u001b[33m"
+        @blue = "\u001b[34m"
+        @reset = "\u001b[0m"
+    end
+
+    # puts intro script to the rider
+    def intro
+        puts " "
+        puts "#{@yellow}!!!! RIDE FAST CRASH LESS !!!!"
+        puts "       Welcome Riders!#{@reset}"
+        puts " "
+        puts "Ride Fast Crash Less is a tool that allows you to input your city and get back all the best trails in your area."
+        puts "Below is a list of featured cities.  You can either choose one of those by inputing the corresponding number or you can enter your local city."
+        puts " "
+    end
+
+    def featured_cities
+        TrailFinder::Cities.featured_cities
+    end
+
+    def where
+        puts " "
+        puts "#{@green}Where would you like to ride? Enter exit to quit.#{@reset}"
+    end
+
+    def where_now
+        puts "#{@green}* Would you like to find more trails? *"
+        puts "Enter a new city to search! Or enter Exit to hit the trails!#{@reset}"
+    end
+
+    def city_true(input)
+        TrailFinder::Cities.all.map {|cities| cities.city}.include?(input)
+    end
+
+    def get_trails(city, state = nil, distance = nil)
+        item = TrailFinder::Cities.get_city(city, state, distance)
+        TrailFinder::Trails.destroy
+        TrailFinder::Trail_Scrape.api_call(item.coordinates[0], item.coordinates[1], distance)
+        TrailFinder::Trails.all.each.with_index(1) do |trails, i|
+            puts "#{i}. #{trails.name}"
+        end
+    end
+
+    def get_trail_details(choice)
+        TrailFinder::Trails.all.select.with_index(1) do |trails, i|
+            if i == choice.to_i
+                puts "#{trails.name}"
+            end
+        end
+    end
+
+    def controller_state
+        puts "#{@green}What State is the city in?#{@reset}"
+        @input_state = gets.strip.split.map(&:capitalize).join(" ")
+        location = TrailFinder::Cities.all.map {|cities| cities.city == @input_city2 && cities.state == @input_state}
+        if location == []
+            puts " "
+            puts "#{@red}We couldn't find #{@input_city2}, #{@input_state} in our database.  Try again or enter the nearest major city.#{@reset}"
+            controller_main
+        else
+            controller_distance
+        end
+    end
+
+    def controller_distance
+        puts "#{@green}How far from your city would you like to search#{@reset}"
+        @input_distance = gets.strip
+        puts get_trails(@input_city2, @input_state, @input_distance)
+        controller_details
+    end
+
+    def controller_details
+        puts "#{@green}would you like more details on any of these trails? (y/n)#{@reset}"
+        input = gets.strip.downcase
+        if input == "y" || input == "yes"
+            puts "#{@green}Please enter the number of the trail you would like more info on#{@reset}"
+            input2 = gets.strip.downcase
+            if input2.to_i.between?(1, TrailFinder::Trails.all.count)
+                get_trail_details(input2)
+                controller_details
+            else
+                puts "#{@red}There isn't a trail with that input#{@reset}"
+                puts " "
+                controller_details
+            end
+        elsif input == "n"
+            puts ""
+        else
+            puts "#{@red}Please enter (y/n).#{@reset}"
+            puts " "
+            controller_details
+        end 
+    end
+
+    def over
+        puts "#{@yellow}Thanks for riding with us today!#{@reset}"
+        puts " "
+    end
+
+    def controller_main
+        @input_city = nil
+        @input_city2 = nil
+        @counter = 0
+        while @input_city != "Exit"
+            if @counter == 0
+                where
+            else
+                where_now
+            end
+        @input_city = gets.strip.split.map(&:capitalize).join(" ")
+            if city_true(@input_city) == true
+                @input_city2 = @input_city
+                @input_city = "Good Input"
+            end
+            case @input_city
+            when "1"
+                puts get_trails(1)
+                controller_details
+            when "2"
+                puts get_trails(2)
+                controller_details
+            when "3"
+                puts get_trails(3)
+                controller_details
+            when "4"
+                puts get_trails(4)
+                controller_details
+            when "5"
+                puts get_trails(5)
+                controller_details
+            when "Good Input"
+                controller_state
+            else
+                if @input_city != "Exit"
+                    puts " "
+                    puts "#{@red}Sorry we can't find that city in our database.  Please check the spelling, or use the nearest major city.#{@reset}"
+                end
+            end
+            @counter =+ 1
+        end
+    end
+
 end
+
+    
+         
 
 ################################################################################################################
 # #CLI Controller
@@ -131,28 +277,28 @@ end
 #     end
 
 #     #handles asking the rider for input on trail details and puts those trail details out to the rider
-#     def controller_details
-#         puts "#{@green}would you like more details on any of these trails? (y/n)#{@reset}"
-#         input = gets.strip.downcase
-#         if input == "y" || input == "yes"
-#             puts "#{@green}Please enter the number of the trail you would like more info on#{@reset}"
-#             input2 = gets.strip.downcase
-#             if input2.to_i.between?(1, TrailFinder::Get_Trails.list[0].trail_count)
-#                 get_trail_details(input2)
-#                 controller_details
-#             else
-#                 puts "#{@red}There isn't a trail with that input#{@reset}"
-#                 puts " "
-#                 controller_details
-#             end
-#         elsif input == "n"
-#             puts ""
-#         else
-#             puts "#{@red}Please enter (y/n).#{@reset}"
-#             puts " "
-#             controller_details
-#         end 
-#     end
+    # def controller_details
+    #     puts "#{@green}would you like more details on any of these trails? (y/n)#{@reset}"
+    #     input = gets.strip.downcase
+    #     if input == "y" || input == "yes"
+    #         puts "#{@green}Please enter the number of the trail you would like more info on#{@reset}"
+    #         input2 = gets.strip.downcase
+    #         if input2.to_i.between?(1, TrailFinder::Get_Trails.list[0].trail_count)
+    #             get_trail_details(input2)
+    #             controller_details
+    #         else
+    #             puts "#{@red}There isn't a trail with that input#{@reset}"
+    #             puts " "
+    #             controller_details
+    #         end
+    #     elsif input == "n"
+    #         puts ""
+    #     else
+    #         puts "#{@red}Please enter (y/n).#{@reset}"
+    #         puts " "
+    #         controller_details
+    #     end 
+    # end
 
 #     #exit message to the rider
 #     def over
@@ -161,47 +307,47 @@ end
 #     end
 
 #     #Main controller that handles the process flow of the program.
-#     def controller_main
-#         @input_city = nil
-#         @input_city2 = nil
-#         @counter = 0
-#         while @input_city != "Exit"
-#             if @counter == 0
-#                 where
-#             else
-#                 where_now
-#             end
-#         @input_city = gets.strip.split.map(&:capitalize).join(" ")
-#             if city_true(@input_city) == true
-#                 @input_city2 = @input_city
-#                 @input_city = "Good Input"
-#             end
-#             case @input_city
-#             when "1"
-#                 puts get_trails(1)
-#                 controller_details
-#             when "2"
-#                 puts get_trails(2)
-#                 controller_details
-#             when "3"
-#                 puts get_trails(3)
-#                 controller_details
-#             when "4"
-#                 puts get_trails(4)
-#                 controller_details
-#             when "5"
-#                 puts get_trails(5)
-#                 controller_details
-#             when "Good Input"
-#                 controller_state
-#             else
-#                 if @input_city != "Exit"
-#                     puts " "
-#                     puts "#{@red}Sorry we can't find that city in our database.  Please check the spelling, or use the nearest major city.#{@reset}"
-#                 end
-#             end
-#             @counter =+ 1
-#         end
-#     end
+    # def controller_main
+    #     @input_city = nil
+    #     @input_city2 = nil
+    #     @counter = 0
+    #     while @input_city != "Exit"
+    #         if @counter == 0
+    #             where
+    #         else
+    #             where_now
+    #         end
+    #     @input_city = gets.strip.split.map(&:capitalize).join(" ")
+    #         if city_true(@input_city) == true
+    #             @input_city2 = @input_city
+    #             @input_city = "Good Input"
+    #         end
+    #         case @input_city
+    #         when "1"
+    #             puts get_trails(1)
+    #             controller_details
+    #         when "2"
+    #             puts get_trails(2)
+    #             controller_details
+    #         when "3"
+    #             puts get_trails(3)
+    #             controller_details
+    #         when "4"
+    #             puts get_trails(4)
+    #             controller_details
+    #         when "5"
+    #             puts get_trails(5)
+    #             controller_details
+    #         when "Good Input"
+    #             controller_state
+    #         else
+    #             if @input_city != "Exit"
+    #                 puts " "
+    #                 puts "#{@red}Sorry we can't find that city in our database.  Please check the spelling, or use the nearest major city.#{@reset}"
+    #             end
+    #         end
+    #         @counter =+ 1
+    #     end
+    # end
 # end
 
