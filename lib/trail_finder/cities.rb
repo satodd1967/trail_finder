57142b565fc3f74ec2b433a19c45dda88f68048d
satodd1967 <seth.todd@outlook.com>
2020-05-21 18:58:01 -0500

    changed the f_cities class variable to featured_cities to make it easier to read and understand

--- lib/trail_finder/cities.rb
+++ lib/trail_finder/cities.rb
@@ -3,7 +3,7 @@ class TrailFinder::Cities
     attr_accessor :city, :coordinates, :state, :rank, :growth_from_2000_to_2013, :population
    
     @@all = []
-    @@f_cities = []
+    @@featured_cities = []
 
     #initializes an instance of a city for each city pushed into the class from the scraper class based on the provided hash
     def initialize(city_list)
@@ -18,7 +18,7 @@ class TrailFinder::Cities
 
     #reader for the @@f_cities class variable.  This variable keeps track of the random cities chosen for featured cities
     def self.f_cities
-        @@f_cities
+        @@featured_cities
     end
 
     #pulls 5 random cities from the cities instances and then saves those cities to the @@f_cities class variable for later use in the details methods
@@ -28,15 +28,15 @@ class TrailFinder::Cities
             puts "#{i}. #{cities.city}, #{cities.state}"
         end
         sample.each do |cities|
-            @@f_cities << cities
+            @@featured_cities << cities
         end
     end
 
     #provides the appropriate city instance to the get trails method in the cli.  This could be based on a featured city or a user entered city
     def self.get_city(city, state = nil, distance = nil)
-        if city.to_i.between?(1, @@f_cities.count)
+        if city.to_i.between?(1, @@featured_cities.count)
             input = city.to_i - 1
-            item = @@f_cities[input]
+            item = @@featured_cities[input]
         else 
             item = @@all.find {|cities| cities.city == city && cities.state = state}
         end
